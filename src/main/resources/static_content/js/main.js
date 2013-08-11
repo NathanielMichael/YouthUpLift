1bccac906bd566068ed25bac2c96d07429166b19
krogebry <bryan_kroger@htc.com>
2013-08-10 17:16:57 -0700

    Tweaking javascript.

---
+++ src/main/resources/static_content/js/main.js
@@ -0,0 +1,34 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: bryan_kroger
+ * Date: 8/10/13
+ * Time: 4:53 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+function YouthUpLift{
+
+    var key = "";
+    var value = "value";
+
+    function yada = function( key ){
+        console.log( "yada: " + this.key );
+    }
+
+    function getUser = function( user_id ){
+        console.log( "Creating user" );
+    }
+
+    function getUsers = function(){
+        console.log( "Getting users" );
+    }
+
+    function getProfiles = function(){
+        console.log( "Getting profiles" );
+    }
+
+    function getProfile = function( profile_id ){
+        console.log( "Getting profile: " +profile_id );
+    }
+
+}
